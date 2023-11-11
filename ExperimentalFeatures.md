ba0a327e7f21866c36363abb89e9b2d80be6ad25
hudson-newey <hudsonnewey@gmail.com>
2023-11-11 23:37:45 +1000

    Added functional operators

---
+++ ExperimentalFeatures.md
@@ -0,0 +1,64 @@
+# Experimental Features
+
+## Functional operatorators
+
+```html
+    <input
+      id="number-input"
+      type="number"
+      onchange="app.numberOutput = event.target.value"
+      oninput="app.numberOutput = event.target.value"
+    />
+    <h1>Double your number is: <span @numberOutput></span></h1>
+
+    <script>
+      const app = new Component({
+        numberOutput: (x) => x * 2,
+      });
+    </script>
+```
+
+Every time `numberOutput` is updated, the function is applied to the new value
+
+### Alternatives
+
+```html
+    <input
+      id="number-input"
+      type="number"
+      onchange="numberOutput(event.target.value)"
+      oninput="numberOutput(event.target.value)"
+    />
+    <h1>Double your number is: <span @numberOutput></span></h1>
+
+    <script>
+      const app = new Component({
+        numberOutput: (x) => x * 2,
+      });
+    </script>
+```
+
+I don't like this approach as want to seperate class methods (called through `onclick='foo()'`) and stateful changes.
+
+In my eyes, all stateful changes should be done through `x = y`, and functions with side effects should be `onclick='foo()'` where `foo` looks something like:
+
+```html
+<button onclick="app.foo()">Click Me!</button>
+
+<script>
+    const app = new Component({
+        foo: () => alert("Hello World!"),
+    });
+</script>
+```
+
+## Flow dependencies (not implemented)
+
+```js
+const app = new Component({
+    foo: 5,
+    bar: () => a * 2,
+});
+```
+
+Updating `foo` should also update references to `bar` (I think)
--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 class Component {
   constructor(data) {
     Object.keys(data).forEach((key) => {
-      let privateValue = this[key];
-      
+      let privateValue = data[key];
+
+      const identityFunction = (x) => x;
+      const operatorFunction =
+        typeof privateValue === "function" ? privateValue : identityFunction;
+
       Object.defineProperty(this, key, {
+        // called when methods are called
         get() {
           return privateValue;
         },
         set(value) {
-          privateValue = value;
+          privateValue = operatorFunction(value);
 
           // binding by DOM attributes
-          if (key.startsWith("#") || key.startsWith(".") || key.startsWith("[")) {
+          if (
+            key.startsWith("#") ||
+            key.startsWith(".") ||
+            key.startsWith("[")
+          ) {
             const elements = document.querySelectorAll(key);
 
             elements.forEach((element) => {
-              element.innerHTML = value;
+              element.innerHTML = privateValue;
             });
 
             return;
@@ -24,10 +33,17 @@ class Component {
           // custom @key binded attributes
           const modelElements = document.querySelectorAll(`[\\@${key}]`);
           modelElements.forEach((element) => {
-            element.innerHTML = value;
+            element.innerHTML = privateValue;
           });
         },
       });
+    });
+
+    // remove all functional properties from the data object
+    Object.keys(data).forEach((key) => {
+      if (typeof data[key] === "function") {
+        delete data[key];
+      }
     });
 
     // we do this last so that it triggers the setters
