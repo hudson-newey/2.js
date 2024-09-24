3048f26d579323132def9b6c4b355d36dcbd788c
hudson-newey <grath@duck.com>
2024-09-24 21:41:26 +1000

    Add cjs, and umd to dist output

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,19 @@ import dts from "vite-plugin-dts";
 export default defineConfig({
     plugins: [dts()],
     build: {
+        lib: {
+            name: "2js",
+            fileName: "2js",
+            entry: "src/2js.ts",
+            formats: ["es", "cjs", "umd"],
+        },
         minify: "terser",
         terserOptions: {
             mangle: {
                 reserved: ["Component"],
             },
             compress: true,
-            toplevel: true
-        },
-        lib: {
-            name: "2js",
-            fileName: "2js",
-            entry: "src/2js.ts",
-            formats: ["es"],
+            toplevel: true,
         },
     },
 });
